8a3ccd665c6fa7380f3e5b900849dcd80f15e3be
Anton <bur2000_2010@mail.ru>
2021-01-27 16:29:19 +0400

    подключение material UI

--- first-app/src/Ccomponent.js
+++ first-app/src/Ccomponent.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './style.css';
+import { Button } from '@material-ui/core';
 //!Контролируемый input
 export default class Ccomponent extends Component { //export для экспортирование компонента
     constructor(props) {
@@ -22,20 +24,31 @@ export default class Ccomponent extends Component { //export для экспор
        event.preventDefault()//обработчик отправки, без перезагрузки страницы
        this.setState({
             input: this.state.input,
-            items: [...this.state.items, this.state.input]
+            items: [...this.state.items, this.state.input]//спред оператор es6, добавляет значение состояния input в массив items
        });
     }
 
     render() {
+        // const styles={
+        //     color:'brown', 
+        //     backgroundColor:'yellow',
+        //     borderRadius:8,
+        //     fontSize:'1.4em'
+        // }
        return(
            <div>
+               <h1 className='hello'> Hello,world</h1>
+                <Button variant="contained" color="primary">
+                    Primary
+                </Button>
+               {/*<h1 style={styles}> Hello,world</h1>{/*в реакт ожидается не стоковое значение а объект содержащий пару ключ и значение, где ключ-имя css*/}
                <form onSubmit={this.handleSubmit}>
                  <input value={this.state.input} onChange ={this.handleChange}/>
                  <button type='submit'>submit</button>
                </form>
 
             <ul>
-                {this.state.items.map((item,index)=>(
+                {this.state.items.map((item,index)=>(//map выводит массив
                     <li key={index}>{item}</li>
                 ))}
             </ul>
