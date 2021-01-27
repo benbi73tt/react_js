718200a3b9b140bf8455875cabda3e9b4db62ad3
Anton <bur2000_2010@mail.ru>
2021-01-27 16:52:35 +0400

    Передача данных между компонентами 1.

--- first-app/src/Ccomponent.js
+++ first-app/src/Ccomponent.js
@@ -1,61 +1,27 @@
 import React, { Component } from 'react';
 import './style.css';
-import { Button } from '@material-ui/core';
+// import { Button } from '@material-ui/core';
+import Fcomponent from './Fcomponent';
 //!Контролируемый input
 export default class Ccomponent extends Component { //export для экспортирование компонента
     constructor(props) {
         super(props);
     
         this.state = {
-            input:'',
-            items: []
-        };
-        this.handleChange=this.handleChange.bind(this);//привязать метод по умолчанию
-        this.handleSubmit=this.handleSubmit.bind(this);
+            name:'web dev blog'
+      }
     }
-
-    handleChange(event){
-        this.setState({
-            input: event.target.value
-        })
-    }
-
-    handleSubmit(event){//устанавливает свойство состояния
-       event.preventDefault()//обработчик отправки, без перезагрузки страницы
-       this.setState({
-            input: this.state.input,
-            items: [...this.state.items, this.state.input]//спред оператор es6, добавляет значение состояния input в массив items
-       });
-    }
+  
 
     render() {
-        // const styles={
-        //     color:'brown', 
-        //     backgroundColor:'yellow',
-        //     borderRadius:8,
-        //     fontSize:'1.4em'
-        // }
        return(
            <div>
-               <h1 className='hello'> Hello,world</h1>
-                <Button variant="contained" color="primary">
-                    Primary
-                </Button>
-               {/*<h1 style={styles}> Hello,world</h1>{/*в реакт ожидается не стоковое значение а объект содержащий пару ключ и значение, где ключ-имя css*/}
-               <form onSubmit={this.handleSubmit}>
-                 <input value={this.state.input} onChange ={this.handleChange}/>
-                 <button type='submit'>submit</button>
-               </form>
-
-            <ul>
-                {this.state.items.map((item,index)=>(//map выводит массив
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
+            <p>hello youtube {this.state.name}</p>
+            <Fcomponent nameds={this.state.name}/>
            </div>
-       )
-    };
-}
+        )
+        }
+    }
 
 
 //!rcc классовые компоненты
--- first-app/src/Fcomponent.js
+++ first-app/src/Fcomponent.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function fcomponent(props) {//export для экспортирование компонента
-    return (
-        <div>
-            <h1>Hello,{props.name} {props.surname}</h1>
-        </div>
-    )
+export default class fcomponent extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Hello, {this.props.nameds}</h1>
+            </div>
+        )
+    }
 }
+
 //! rfc функциональный компонент
 //props(свойства) - это значение которые мы статически, сразу хотим передать компоненту
