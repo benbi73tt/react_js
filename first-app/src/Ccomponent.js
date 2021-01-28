37ba62b1d3fdfcdf7b3251a6825a4a907b0690bf
Anton <bur2000_2010@mail.ru>
2021-01-28 11:07:40 +0400

    Маршутизация по страницам
    Link
    Router
    react-router-dom

--- first-app/src/Ccomponent.js
+++ first-app/src/Ccomponent.js
@@ -2,61 +2,61 @@ import React, { Component } from 'react';
 import './style.css';
 // import { Button } from '@material-ui/core';
 // import Fcomponent from './Fcomponent';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import About from './About';
+import Home from './Home';
+import Users from './users';
 //!Контролируемый input
-export default class Ccomponent extends Component { //export для экспортирование компонента
+
+export default class Ccomponent extends Component {
     constructor(props) {
-        super(props);
+        super(props)
     
         this.state = {
-            error:null, //отслеживает ошибки
-            isLoaded:false,
-            items:[]//набор компонентов, которые будем получать
-      };
+             
+        }
     }
+    
+    render() {
+        return (
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to='/about'>About</Link> {/*to - путь перехода(адресная строка)*/ }
+                            </li>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/users'>Users</Link>
+                            </li>
+                        </ul>
+                    </nav>
 
-    componentDidMount() {
-        fetch('http://ссылка')
-        .then(res => res.json())
-        .then(
-            (result)=>{
-                this.setState({  //Здесь изменяем состояние
-                    isLoaded: true,
-                    items: result.drinks//(drins-название главное json)
-                });
-                     
-            },
-            (error)=>{
-                this.setState({  //Здесь изменяем состояние
-                    isLoaded: true,
-                   error
-                 });
-            }
+                    <Switch> {/*далее внутри switch каждый маршрут предосталвяет объект Route*/}
+                         <Route path='/About'>
+                            <About/>
+                         </Route>
+                         <Route path='/'>
+                            <Home/>
+                         </Route>
+                         <Route path='/users'>
+                            <Users/>
+                         </Route>
+                    </Switch>
+                </div>
+            </Router>
         )
     }
-  
+}
 
-    render() {
-        const {error, isLoaded,items}=this.state;
-        if(error){
-            return (<p>Error {error.message}</p>)
-        }else if(!isLoaded){
-            return <p>Loading...</p>
-        }else{
-            return (
-                <ul>
-                    {items.map(item=>(
-                        <li key={item.name}>
-                            {item.strDrink}
-                            <img  width='50' height='50' src ={item.strDrinkThumb}/>
-                        </li>
-                    ))}
-                
-                </ul>
-                
-            )
-        }
-        }
-    }
 
 
 //!rcc классовые компоненты
