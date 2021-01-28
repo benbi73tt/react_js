f762a4600756d427025690d5e3c200413f92528e
Anton <bur2000_2010@mail.ru>
2021-01-28 16:30:17 +0400

    Вывод массива списком

--- first-app/src/Ccomponent.js
+++ first-app/src/Ccomponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './style.css';
-import { Button } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
+// import { Button } from '@material-ui/core';
+// import TextField from '@material-ui/core/TextField';
 // import {
 //     BrowserRouter as Router,
 //     Switch,
@@ -16,54 +16,22 @@ export default class Ccomponent extends Component {
         super(props)
     
         this.state = {
-            TextField :'',
-            submit:''
             
         }
-        this.handleChange=this.handleChange.bind(this);
-        this.handleSubmit=this.handleSubmit.bind(this);
     }
 
-    handleChange(event){
-        this.setState({
-         TextField : event.target.value
-        });
-     }
-
-    handleSubmit(event){
-        event.preventDefault();
-        this.setState({
-         submit: this.state.TextField 
-        });
-     }
 
 
 
     
     render() {
-        
+        const arr=['a','b','c','d','e'];
+        const list=arr.map(function(item,index){
+                  return <li key={index}>{item}</li>
+        });
         return (
           <div>
-             <form onSubmit={this.handleSubmit}>
-                <h1>Who are you?</h1>
-                <TextField 
-                    value={this.state.TextField } 
-                    onChange ={this.handleChange}
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="name"
-                    label="name"
-                    name="name"
-                    autoComplete="name"
-                    autoFocus
-                /> 
-                    <Button  variant="contained" color="primary" type='submit'>
-                    Primary
-                </Button>
-               </form>
-               <h3>hello, {this.state.submit}</h3>
+              <ul>{list}</ul>
                
 
         </div>
