a8b3806cb7b961a630cd431a3bece70186ca6819
Anton <bur2000_2010@mail.ru>
2021-02-01 12:54:50 +0400

    Ссылки, списки,
    {item} {index}
    <li key={index}><a href={item.href}>{item.text} {index+1}</a></li>

--- first-app/src/Ccomponent.js
+++ first-app/src/Ccomponent.js
@@ -19,7 +19,13 @@ export default class Ccomponent extends Component {
             name:'Иван',
             age:25,
             flag:true,
-            show:true
+            show:true,
+            items:['Вася','Петя','Коля'],
+            hrefs: [
+                {href: '1.html', text: 'ссылка'},
+                {href: '2.html', text: 'ссылка'},
+                {href: '3.html', text: 'ссылка'},
+            ]        
             
         }
         this.ChangeUsers=this.ChangeUsers.bind(this);
@@ -58,13 +64,21 @@ ShowText(){
 
     
     render() {
+        const text = <p>{this.state.show?<p>{this.state.flag?'Привет':'Пока'} {this.state.name} {this.state.age}</p>:" "}</p>;
+        const list= this.state.items.map(function(item,index){
+            return <li key={index}> {index+1} - {item} </li>
+        })
+        const listA=this.state.hrefs.map((item,index)=>{
+            return <li key={index}><a href={item.href}>{item.text} {index+1}</a></li>
+        })
         return (
           <div>
-              
-              <p>{this.state.show?<p>{this.state.flag?'Привет':'Пока'} {this.state.name} {this.state.age}</p>:" "}</p>
+              {text}
               <button onClick={this.ChangeUsers}>Смена</button>
               <button onClick={this.BtnHi}>{this.state.flag?'Попращаться':'Поприветствовать'}</button>
               <button onClick={this.ShowText}>{this.state.show?'Скрыть':'Показать'}</button>
+              <ul>{list}</ul>
+              <ul>{listA}</ul>
 
         </div>
         )
