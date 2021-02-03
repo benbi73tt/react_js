11de8405c038adf11b287091498b868a6636de91
Anton <bur2000_2010@mail.ru>
2021-02-03 23:24:46 +0400

    checkbox

--- first-app/src/Ccomponent.js
+++ first-app/src/Ccomponent.js
@@ -16,53 +16,30 @@ export default class Ccomponent extends Component {
         super(props)
     
         this.state = { 
-            users: [
-                {name: 'Коля', age: 30},
-                {name: 'Вася', age: 40},
-                {name: 'Петя', age: 50},
-            ]
+            value:'',
+            checked:true
         }   
 
-        this.handleChange1=this.handleChange1.bind(this);
-        this.handleChange2=this.handleChange2.bind(this);
-        this.handleSubmit=this.handleSubmit.bind(this);
+        this.handleCheckbox=this.handleCheckbox.bind(this);
+
     }
 
-handleChange1(event){
-    this.setState({
-        name:event.target.value
-    })
-}
-handleChange2(event){
-    this.setState({
-        age:event.target.value
-    })
-}
-
-handleSubmit(event){
-    event.preventDefault();
-    this.setState({
-        users:[...this.state.users,{name:this.state.name,age:this.state.age}]
-    })
-}
+    handleCheckbox(event){
+        this.setState({
+            checked:!this.state.checked
+        })
+    }
 
  
 
     
     render() {
-        const table=this.state.users.map((item,index)=>{
-            return(<tr key={index}><td>{index+1}. {item.name}</td><td>{item.age}</td></tr>)
-        })
+        const text=this.state.checked?<p>{'Hello, my friend'}</p>:' ';
+
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <table>{table}</table>
-                    <input onChange={this.handleChange1} placeholder='Имя'/><br/>
-                    <input onChange={this.handleChange2} placeholder='Возраст'/><br/>
-                    <button type='submit'>submit</button>
-                </form>
-
-
+                <input checked={this.state.checked} onChange={this.handleCheckbox} type='checkbox'/>
+                {text}
 
             </div>
         )
