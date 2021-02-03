bac4cb50e2946292e7308222b76cbd422fe43b8c
Anton <bur2000_2010@mail.ru>
2021-02-03 10:34:14 +0400

    3 input(Ф.И.О) вывести в h3

--- first-app/src/Ccomponent.js
+++ first-app/src/Ccomponent.js
@@ -23,6 +23,7 @@ export default class Ccomponent extends Component {
         }   
         this.handleChange1=this.handleChange1.bind(this);
         this.handleChange2=this.handleChange2.bind(this);
+        this.handleChange3=this.handleChange3.bind(this);
         this.Sum=this.Sum.bind(this);
     }
 
@@ -36,10 +37,15 @@ export default class Ccomponent extends Component {
             number2:event.target.value,
         })
     }
+    handleChange3(event){
+        this.setState({
+            number3:event.target.value,
+        })
+    }
 Sum(event){
     event.preventDefault();
     this.setState({
-        submit:+this.state.number1+(+this.state.number2)
+        submit:this.state.number1+ ' '+this.state.number2+' '+this.state.number3
 
     })
 }
@@ -49,11 +55,12 @@ Sum(event){
         const num=this.state.submit;
         return (
             <div>
-                <h3>Сумма</h3>
+                <h3>Фио</h3>
                 <form onSubmit={this.Sum}>
-                    <input value={this.state.value} onChange={this.handleChange1}/>
-                    <input value={this.state.value} onChange={this.handleChange2}/>
-                    <button type='submit'>Sum</button>
+                    <input placeholder='Фамилия' onChange={this.handleChange1}/><br/>
+                    <input placeholder='Имя'  onChange={this.handleChange2}/><br/>
+                    <input placeholder='Отчество'  onChange={this.handleChange3}/><br/>
+                    <button type='submit'>Enter</button>
                 </form>
                 <h3>{num}</h3>
 
