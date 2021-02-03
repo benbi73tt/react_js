273d1ab973c0983bd2ed8135b699da8c86939d20
Anton <bur2000_2010@mail.ru>
2021-02-03 22:31:42 +0400

    Дабовление в table

--- first-app/src/Ccomponent.js
+++ first-app/src/Ccomponent.js
@@ -16,76 +16,50 @@ export default class Ccomponent extends Component {
         super(props)
     
         this.state = { 
-            hrefs: [
-                {href: '1.html', text: 'ссылка'},
-                {href: '2.html', text: 'ссылка'},
-                {href: '3.html', text: 'ссылка'},
-            ],
+            users: [
+                {name: 'Коля', age: 30},
+                {name: 'Вася', age: 40},
+                {name: 'Петя', age: 50},
+            ]
         }   
+
         this.handleChange1=this.handleChange1.bind(this);
         this.handleChange2=this.handleChange2.bind(this);
-        this.handleChange3=this.handleChange3.bind(this);
         this.handleSubmit=this.handleSubmit.bind(this);
-        this.NewList=this.NewList.bind(this);
-        this.DelList=this.DelList.bind(this);
-
     }
 
-    handleChange1(event){
-        this.setState({
-            href:event.target.value
-        })
-    }
+handleChange1(event){
+    this.setState({
+        name:event.target.value
+    })
+}
+handleChange2(event){
+    this.setState({
+        age:event.target.value
+    })
+}
 
-    handleChange2(event){
-        this.setState({
-            text:event.target.value
-        })
-    }
-    handleChange3(event){
-        this.setState({
-            number:event.target.value
-        })
-    }
-
-    handleSubmit(event){
-        event.preventDefault();
-        // this.state.hrefs.push({href:this.state.href,text:this.state.text})
-
-    }
-
-    NewList(){
-        this.setState({
-            hrefs:[...this.state.hrefs,{href:this.state.href,text:this.state.text}]
-        })
-    }
-
-    DelList(number){
-        this.state.hrefs.splice(number,1)
-        this.setState({
-            hrefs:this.state.hrefs
-
-        })
-    }
-
+handleSubmit(event){
+    event.preventDefault();
+    this.setState({
+        users:[...this.state.users,{name:this.state.name,age:this.state.age}]
+    })
+}
 
  
 
     
     render() {
-        const list=this.state.hrefs.map((item,index)=>{
-            return(<li key={index}><a href={item.href}>{item.text}-{index+1}</a></li>)
+        const table=this.state.users.map((item,index)=>{
+            return(<tr key={index}><td>{index+1}. {item.name}</td><td>{item.age}</td></tr>)
         })
-        const number=this.state.number-1;
         return (
             <div>
-                <form onSubmit={this.handleSubmit} >
-                    <ul>{list}</ul>
-                    <input onChange={this.handleChange1}/>
-                    <input onChange={this.handleChange2}/>
-                    <button type='submit' onClick={this.NewList}>submit</button><br/>
-                    <input onChange={this.handleChange3}/>
-                    <button type='submit' onClick={this.DelList.bind(this,number)}>Удалить</button>
+                <form onSubmit={this.handleSubmit}>
+                    <table>{table}</table>
+                    <input onChange={this.handleChange1} placeholder='Имя'/><br/>
+                    <input onChange={this.handleChange2} placeholder='Возраст'/><br/>
+                    <button type='submit'>submit</button>
                 </form>
 
 
