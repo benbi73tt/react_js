62986f439ed5fedb06b10957582afacd56502811
Anton <bur2000_2010@mail.ru>
2021-02-04 11:52:16 +0400

    Заполнение массива и выовод
    textarea

--- first-app/src/Ccomponent.js
+++ first-app/src/Ccomponent.js
@@ -15,54 +15,49 @@ export default class Ccomponent extends Component {
     constructor(props) {
         super(props)
     
-        this.state = { 
-            value:'Ульяновск',
-            town:['Ульяновск','Москва','Питер','Казань'],
-            option:''
-        }   
-
+        this.state = {
+            texts:[],
+        }
         this.handleChange=this.handleChange.bind(this);
-        this.handleRadioChange=this.handleRadioChange.bind(this);
-
+        this.handleSubmit=this.handleSubmit.bind(this);
     }
 
     handleChange(event){
         this.setState({
-            value:event.target.value
-        })
-    }
-    handleRadioChange(event){
-        this.setState({
-            option:event.target.value
+            text:event.target.value
         })
     }
 
+    handleSubmit(event){
+        event.preventDefault();
+        event.target.reset();
+        if(this.state.text==='')return;
+        else{        
+            this.setState({
+            texts:[...this.state.texts,this.state.text],
+            text:''})}
 
- 
 
+    }
     
     render() {
-        const town=this.state.town.map((item,index)=>{
-            return(<option key={index}>{item}</option>)
+        const text=this.state.texts.map((item,index)=>{
+            return(<p key={index}>{item}</p>)
         })
-
         return (
             <div>
-                <select onChange={this.handleChange}>
-                    {town}
-                </select>
-                <h3>{this.state.value}</h3>
-
-                <input onChange={this.handleRadioChange} checked={this.state.option === '1.touch'} value='1.touch' type='radio'/>
-                <input onChange={this.handleRadioChange} checked={this.state.option === '2.touch'} value='2.touch' type='radio'/>
-                <input onChange={this.handleRadioChange} checked={this.state.option === '3.touch'} value='3.touch' type='radio'/>
-                <h3>{this.state.option}</h3>
-
-
+                <form onSubmit={this.handleSubmit}>
+                    <textarea placeholder='Введите текст'value={this.state.value} onChange={this.handleChange}/>
+                    <button type='submit'>submit</button>
+                </form>
+        
+                {text}
+                
             </div>
         )
     }
 }
+
 
 
 
