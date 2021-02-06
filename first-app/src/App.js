d600eada14ee567618e53cd0732d471e90314f91
Anton <bur2000_2010@mail.ru>
2021-02-06 17:51:36 +0400

    Список дел(Ошибка)
    При добавлении выводит 2 дела вместо одного

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-import Ccomponent from './Ccomponent';
+import React from 'react';
+import Task from './component/Task';
+import Taskinput from './component/Taskinput';
 
-function App() {
-    return ( <div className = "App" >
-        <header className = "App-header" > 
-         <Ccomponent / >
-        <img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <p>Edit < code > src / App.js </code> and save to reload. </p > 
-        <a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >ауф</a> 
-        </header > 
-        </div>
-    );
+class App extends React.Component {
+    constructor() {
+        super();
+    
+        this.state = {
+            tasks:[
+                {id:0, title: 'create tido-react app', done:false},
+                {id:1, title: 'Make a video about it', done:true},
+                {id:2, title: 'create simple todo-app', done:false}
+            ]
+             
+        };
+    }
+
+    addTask = task => {
+        this.setState(state =>{
+            let { tasks } = state;
+            tasks.push({
+                id: tasks.length!==0? task.length:0,
+                title: task,
+                done: false
+
+            });
+            return (tasks);
+        });
+    };
+
+    doneTask = id=> {
+        const index = this.state.tasks.map(task => task.id).indexOf(id);
+        this.setState(state => {
+            let {tasks} = state;
+            tasks[index].done = true;
+            return tasks;
+        });
+    };
+
+    delTask=id=>{
+        const index= this.state.tasks.map(task=>task.id).indexOf(id);
+        this.setState(state=>{
+            let {tasks}=state;
+            delete tasks[index];
+            return tasks;
+        });
+
+    };
+    
+    render() {
+        const {tasks}=this.state;
+        const activeTasks=tasks.filter(task=>!task.done);
+        const doneTasks=tasks.filter(task=>task.done);
+
+        return (
+            <div className='App'>
+                <h1 className='top'>Active tasks: {activeTasks.length}</h1>
+                {[...activeTasks,...doneTasks].map(task=>(
+                    <Task 
+                    doneTask={()=>this.doneTask(task.id)} 
+                    delTask={()=>this.delTask(task.id)}
+                    task={task} 
+                    key={task.id}>
+                </Task> 
+            ))}
+            <Taskinput addTask={this.addTask}></Taskinput>
+                    
+            </div>
+        )
+    }
 }
-
+    
 export default App;
