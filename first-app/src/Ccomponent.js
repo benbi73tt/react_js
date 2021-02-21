27ee8d1efb067e0df8915e16a76241db54cb24a5
Anton <bur2000_2010@mail.ru>
2021-02-21 08:49:16 +0400

    Смена дня недели (рус/англ) по выбранному селекту

--- first-app/src/Ccomponent.js
+++ first-app/src/Ccomponent.js
@@ -16,12 +16,24 @@ export default class Ccomponent extends Component {
         super(props)
     
         this.state = {
-            items:[
-                {id:1,name:'anton', surname:'burmistr',age:25, salary: 2000, done:false},
-                {id:0,name:'artem', surname:'ananichev', age:20, salary: 5000, done:false},
-                {id:2,name:'dima', surname:'semen',age:30, salary:4000, done:false},
-                {id:-1,name:'cnton', surname:'zurmistr',age:25, salary: 3000, done:false},
+            arrLang:[
+                {en:'Monday',rus:'Понедельник'},
+                {en:'Tuesday',rus:'Вторник'},
+                {en:'Wednesday',rus:'Среда'},
+                {en:'Thursday',rus:'Четверг'},
+                {en:'Friday',rus:'Пятница'},
+                {en:'Saturday',rus:'Суббота'},
+                {en:'Sunday',rus:'Воскресенье'},
+                
             ],
+            lang:'rus',
+            // items:[
+            //     {id:1,name:'anton', surname:'burmistr',age:25, salary: 2000, done:false},
+            //     {id:0,name:'artem', surname:'ananichev', age:20, salary: 5000, done:false},
+            //     {id:2,name:'dima', surname:'semen',age:30, salary:4000, done:false},
+            //     {id:-1,name:'cnton', surname:'zurmistr',age:25, salary: 3000, done:false},
+            // ],
+            
             // routes:[
             //     {id:0,name:'Ульяновск-Москва'},
             //     {id:1,name:'Москва-Ульяновск'},
@@ -34,77 +46,37 @@ export default class Ccomponent extends Component {
         // this.handleChange=this.handleChange.bind(this);
         // this.handleSubmit=this.handleSubmit.bind(this);
     }
+    handleChange(){
+        if(this.state.lang==='rus')
+        this.setState({
+            lang:'en'
+        })
+        else{
+            this.setState({
+                lang:'rus'
+            })
+        }
 
-    handleSortName(){
-        var obj = [...this.state.items];
-        obj.sort((a,b)=>{
-            if(a.name < b.name) return -1;
-            if(a.name > b.name) return 1;
-        });
-        obj.map((item, i) => (<div key={i}> {item.surname}  
-                              {item.name} {item.salary}</div>))
-        console.log(obj)
-        this.setState({
-            items:obj
-        })
-
-    }
-    
-    handleSortSurname(){
-        var obj = [...this.state.items];
-        obj.sort((a,b)=>{
-            if(a.surname < b.surname) return -1;
-            if(a.surname > b.surname) return 1;
-        });
-        obj.map((item, i) => (<div key={i}> {item.surname}   
-                              {item.name} {item.salary}</div>))
-        console.log(obj)
-        this.setState({
-            items:obj
-        })
-
-    }
-    handleSortSalary(){
-        var obj = [...this.state.items];
-        obj.sort((a,b) => a.salary - b.salary);
-        obj.map((item, i) => (<div key={i}> {item.surname}   
-                              {item.name} {item.salary}</div>))
-        console.log(obj)
-        this.setState({
-            items:obj
-        })
-    }
-    handleSortid(){
-        var obj = [...this.state.items];
-        obj.sort((a,b) => a.id - b.id);
-        obj.map((item, i) => (<div key={i}> {item.surname}   
-                              {item.name} {item.salary}</div>))
-        console.log(obj)
-        this.setState({
-            items:obj
-        })
     }
 
 
 
     
     render() {
-        const list = this.state.items.map(item=>{
-            return(<li key={item.id}>{item.name}, {item.surname}, {item.salary}
-
-            </li>)
-        })
+        const list = this.state.arrLang.map((item,index)=>{
+            return(<option key={index}>{this.state.lang==='en'?item.en:item.rus}
+                </option>)
+                })
 
         return (
             <div>
-                <ul>
+                <select onChange={this.handleChange.bind(this)}>
+                    <option value={this.state.lang}>Английский</option>
+                    <option selected value={this.state.lang}>Русский</option>
+                </select>
+                <select>
                     {list}
-                    <button onClick={this.handleSortName.bind(this)}>сорт по имени</button>
-                    <button onClick={this.handleSortSurname.bind(this)}>сорт по фамилии</button>
-                    <button onClick={this.handleSortSalary.bind(this)}>сорт по зарплате</button>
-                    <button onClick={this.handleSortid.bind(this)}>сорт по id</button>
-  
-                </ul>
+                </select>
 
             </div>
 
